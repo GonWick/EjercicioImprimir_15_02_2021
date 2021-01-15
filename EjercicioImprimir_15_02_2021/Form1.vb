4ae1fe2367b70dc6c92f4f3b69a7ec7d246a7db8
GonWick <gonxza@gmail.com>
2021-01-15 11:46:38 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioImprimir_15_02_2021/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PrintDialog1.Document = PrintDocument1
+        PrintDialog1.PrinterSettings = PrintDocument1.PrinterSettings
+        PrintDialog1.AllowSomePages = True
+
+        If PrintDialog1.ShowDialog = DialogResult.OK Then
+            PrintDocument1.PrinterSettings = PrintDocument1.PrinterSettings
+            PrintDocument1.Print()
+        End If
+
+    End Sub
+End Class
